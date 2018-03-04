9e7cb987c5c12295c5537e99ce0229beb6bb7225
kongchenglc@gmail.com <kongchenglc@gmail.com>
2018-03-04 21:42:17 +0800

    signup.vue

---
+++ src/page/signup.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="container">
+
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#container {
+    padding: 10px;
+}
+
+@media screen and (min-width: 600px) {
+    #container {
+        width: 450px;
+        margin: 0 auto;
+    }
+}
+</style>
