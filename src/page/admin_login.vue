dba750937f9b0b8479fcdf132a4d311db6526fa4
kongchenglc@gmail.com <kongchenglc@gmail.com>
2018-03-04 21:41:41 +0800

    admin_login.vue

---
+++ src/page/admin_login.vue
@@ -0,0 +1,102 @@
+<template>
+    <div id="container">
+
+        <h1 id="subTitle">ThoughtCoding</h1>
+
+        <div class="input-item">
+            <i class="fa fa-user-o"></i>
+            <input type="text" required placeholder="请输入管理员账号" v-model="aNumber"></input>
+        </div>
+
+        <div class="input-item">
+            <i class="fa fa-key"></i>
+            <input type="password" required placeholder="请输入密码" v-model="aPassword"></input>
+        </div>
+
+        <div id="thesubm" class="input-item" @click="submit">
+            <input class="itssub" type="submit" value="登录"></input>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           aNumber: '',
+           aPassword: ''
+        }
+    },
+    methods: {
+        submit() {
+            this.axios({
+                data: {
+                    aNumber: this.aNumber,
+                    aPassword: this.aPassword,
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+#subTitle {
+    text-align: center;
+    margin: 60px 0 50px 0;
+    font-weight: 500;
+}
+
+#container {
+    padding: 10px;
+}
+
+
+#thesubm {
+    margin-top: 50px;
+    cursor: pointer;
+    border-color: #2CAC62;
+    background-color: #2CAC62;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .2);
+    box-shadow: 0 7px 30px 10px rgba(255, 255, 255, .2) inset, 0 1px 4px 0 rgba(0, 0, 0, .2);
+}
+
+input {
+    background: transparent;
+    border: none;
+    padding-left: 5px;
+    outline: none;
+    color: #fff;
+    width: 80%;
+    line-height: 35px;
+    font-size: 15px;
+    vertical-align: baseline;
+}
+
+input::placeholder {
+    color: rgba(255, 255, 255, .8);
+}
+
+.input-item {
+    padding-left: 15px;
+    margin: 20px 0;
+    border: 1px solid #fff;
+    border-radius: 500px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .2) inset;
+}
+
+.itssub {
+    margin-left: 8%;
+    cursor: pointer;
+}
+
+
+
+@media screen and (min-width: 600px) {
+    #container {
+        width: 450px;
+        margin: 0 auto;
+    }
+}
+</style>
