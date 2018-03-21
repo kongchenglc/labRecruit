45734525d527b8704788dd3b3ab12a34e8392492
kongchenglc@gmail.com <kongchenglc@gmail.com>
2018-03-21 22:06:47 +0800

    store

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import signup from '@/page/signup'
 import success from '@/page/success'
 import message from '@/page/message'
 import admin_login from '@/page/admin_login'
+import admin_messages from '@/page/admin_messages'
+import admin_message from '@/page/admin_message'
 
 Vue.use(Router)
 
@@ -35,6 +37,16 @@ export default new Router({
       path: '/admin_login',
       name: 'Admin Login',
       component: admin_login,
+    },
+    {
+      path: '/admin_messages',
+      name: 'Students Message',
+      component: admin_messages,
+    },
+    {
+      path: '/admin_message',
+      name: 'Student Messages',
+      component: admin_message,
     },
     {
       path: '*',
