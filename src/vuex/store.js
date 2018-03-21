453f9ce4724d116b0a8638daae5259c8bb5c2759
kongchenglc@gmail.com <kongchenglc@gmail.com>
2018-03-21 22:36:20 +0800

    Vuex

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,11 +3,11 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-function set_SessionStorage(theData) {
-    sessionStorage.setItem('userData', JSON.stringify(theData));
+function set_SessionStorage(theData, identity) {
+    sessionStorage.setItem(identity, JSON.stringify(theData));
 }
-function take_sessionStorage() {
-    return JSON.parse(sessionStorage.getItem('userData'));
+function take_sessionStorage(identity) {
+    return JSON.parse(sessionStorage.getItem(identity));
 }
 
 
@@ -16,26 +16,53 @@ export default new Vuex.Store({
         userData: {
         },
         AdminData: {
+        },
+        onesMessage: {
         }
     },
     mutations: {
         setUserData(state, data) {
-            state.userData = data; 
-            set_SessionStorage(data);
+            state.userData = data;
+            set_SessionStorage(data, 'userData');
+        },
+        setAdminData(state, data) {
+            state.AdminData = data;
+            set_SessionStorage(data, 'adminData');
+        },
+        setOnesMessage(state, data) {
+            state.onesMessage = data;
+            set_SessionStorage(data, 'onesMessage');
         }
     },
     getters: {
         getUserData(state) {
-            if(state.userData.sNumber) {
+            if (state.userData.sNumber) {
                 return state.userData;
-            } else if (take_sessionStorage()) {
-                return take_sessionStorage();
+            } else if (take_sessionStorage('userData')) {
+                return take_sessionStorage('userData');
             } else {
-                return {sNumber: 'who is your dad'};
+                return { sNumber: 'who is your dad' };
+            }
+        },
+        getAdminData(state) {
+            if (state.AdminData.identity) {
+                return state.AdminData.identity
+            } else if (take_sessionStorage('adminData')) {
+                return take_sessionStorage('adminData');
+            } else {
+                return '';
+            }
+        },
+        getOnesMessage(state) {
+            if (state.onesMessage.sNumber) {
+                return state.onesMessage
+            } else if (take_sessionStorage('onesMessage')) {
+                return take_sessionStorage('onesMessage');
+            } else {
+                return { sNumber: 'who is your dad??' };
             }
         }
     },
     actions: {
-
     }
 })
